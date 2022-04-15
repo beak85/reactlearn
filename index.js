f79c1019663f855e0a9dcb369f515db31a407768
Matt Geiger <mrgeiger@mrgeiger>
2022-04-15 16:17:59 -0400

    hello change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'matt'});
+    res.send({hi: 'Hello there'});
 });
 
 const PORT = process.env.PORT || 5000;
