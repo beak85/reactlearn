971d62fb3097be15d9a3b57aff7d66c14d9c9040
Matt Geiger <mrgeiger@mrgeiger>
2022-04-18 17:41:52 -0400

    mongoose ready

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,13 @@
-const passport = require('passport');
+const passport = require("passport");
 
 module.exports = (app) => {
-    app.get(
-        '/auth/google', 
-        passport.authenticate('google', {           //GoogleStragey knows 'google'
-            scope: ['profile', 'email']             //user info we want from Google
-        })
-    )
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      //GoogleStragey knows 'google'
+      scope: ["profile", "email"], //user info we want from Google
+    })
+  );
 
-    app.get(
-        '/auth/google/callback',
-        passport.authenticate('google')
-    )
-}
+  app.get("/auth/google/callback", passport.authenticate("google"));
+};
