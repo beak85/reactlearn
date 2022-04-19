c1db8b221e07529f27833b9c5a801d1c95c7fc47
Matt Geiger <mrgeiger@mrgeiger>
2022-04-19 07:49:11 -0400

    auth done

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,19 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model('users');  //pulls the model out of mongoose - one arg
+const User = mongoose.model("users");           //pulls the model out of mongoose - one arg
+
+passport.serializeUser((user, done) => {        //makes cookies from users
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {        //translates cookies back into users
+    User.findById(id)
+    .then(user => {
+        done(null, user)
+    })
+})
+
 
 passport.use(
   new GoogleStrategy(
@@ -13,7 +25,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleID: profile.id }).save();
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+            }
+    });
     }
   )
 );
