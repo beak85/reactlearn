bf27a0a75bdca35e48ee0563316a2d4bc29a35d7
Matt Geiger <mrgeiger@mrgeiger>
2022-04-19 19:16:38 -0400

    dev-prod update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
-  app.get('/app/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
